6bfea770c3a650697a8433c8e7b7a8b144adb5de
Lorenzo <lorenzofg24@gmail.com>
2021-10-19 22:59:10 +0200

    Connection to database done

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const { MONGO_URI } = process.env;
+
+exports.connect = () => {
+  mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log("Successfully connected to database"))
+    .catch((err) => {
+      console.log("Database connection failed. Exiting now...");
+      console.error(err);
+      process.exit(1);
+    });
+};
