93ea66d752025f8882e463ce03a9aa4a456a5012
Lorenzo <lorenzofg24@gmail.com>
2021-10-21 01:34:06 +0200

    Creating POST /api/login endpoint

--- app.js
+++ app.js
@@ -44,4 +44,33 @@ app.post("/api/register", async (req, res) => {
   }
 });
 
+app.post("/api/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!(email && password))
+      return res.status(400).send("All inputs are required");
+
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(404).send("User not found");
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (isPasswordCorrect) {
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        { expiresIn: "2h" }
+      );
+
+      user.token = token;
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = app;
