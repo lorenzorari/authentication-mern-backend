1aa3649ae29f06d1ce5a6a6d98fc9cd7bf7dfc86
Lorenzo <lorenzofg24@gmail.com>
2021-10-21 00:56:38 +0200

    Creating POST /api/register endpoint

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
 app.use(express.json());
+
+const User = require("./model/user");
+
+app.post("/api/register", async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    if (!(firstName && lastName && email && password))
+      return res.status(400).send("All input are required");
+
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) return res.status(400).send("This user already exist");
+
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: encryptedPassword,
+    });
+
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      { expiresIn: "2h" }
+    );
+
+    user.token = token;
+
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = app;
