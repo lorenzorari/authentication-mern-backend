38bbfce86b741869eef61a1adf70e3fffdfe8cc7
Lorenzo <lorenzofg24@gmail.com>
2021-10-22 23:55:27 +0200

    Implementing register() from usersController

--- app.js
+++ app.js
@@ -10,40 +10,9 @@ app.use(express.json());
 
 const auth = require("./middleware/auth");
 const User = require("./model/user");
+const usersController = require("./controllers/users");
 
-app.post("/api/register", async (req, res) => {
-  try {
-    const { firstName, lastName, email, password } = req.body;
-
-    if (!(firstName && lastName && email && password))
-      return res.status(400).send("All input are required");
-
-    const oldUser = await User.findOne({ email });
-
-    if (oldUser) return res.status(400).send("This user already exist");
-
-    encryptedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-      firstName,
-      lastName,
-      email,
-      password: encryptedPassword,
-    });
-
-    const token = jwt.sign(
-      { user_id: user._id, email },
-      process.env.TOKEN_KEY,
-      { expiresIn: "2h" }
-    );
-
-    user.token = token;
-
-    res.status(201).json(user);
-  } catch (err) {
-    console.log(err);
-  }
-});
+app.post("/api/register", usersController.register);
 
 app.post("/api/login", async (req, res) => {
   try {
