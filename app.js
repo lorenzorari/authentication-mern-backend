39b38e660d7fde2cadab0ad3b455a44b9b6cb004
Lorenzo <lorenzofg24@gmail.com>
2021-10-22 23:59:46 +0200

    Cleaning imports

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
 const app = express();
 
 app.use(express.json());
 
 const auth = require("./middleware/auth");
-const User = require("./model/user");
 const usersController = require("./controllers/users");
 
 app.post("/api/register", usersController.register);
