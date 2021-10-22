c07ef4745424607e19e672f61e0a7c52b6ae86f2
Lorenzo <lorenzofg24@gmail.com>
2021-10-23 01:38:02 +0200

    Implementing users routes

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ require("dotenv").config();
 require("./config/database").connect();
 
 const express = require("express");
+const usersRouter = require("./routes/users");
+
 const app = express();
-const withAuth = require("./middleware/auth");
-const usersController = require("./controllers/users");
 
 app.use(express.json());
 
-app.post("/api/users/register", usersController.register);
-
-app.post("/api/users/login", usersController.login);
-
-app.post("/api/users/me", withAuth, usersController.getMe);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
