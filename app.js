cb8b674d116c7c07c7b84120e4b30db5a4976220
Lorenzo <lorenzofg24@gmail.com>
2021-10-22 23:59:23 +0200

    Implementing login() from usersController

--- app.js
+++ app.js
@@ -14,34 +14,7 @@ const usersController = require("./controllers/users");
 
 app.post("/api/register", usersController.register);
 
-app.post("/api/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    if (!(email && password))
-      return res.status(400).send("All inputs are required");
-
-    const user = await User.findOne({ email });
-
-    if (!user) return res.status(404).send("User not found");
-
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
-    if (isPasswordCorrect) {
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        { expiresIn: "2h" }
-      );
-
-      user.token = token;
-    }
-
-    res.status(200).json(user);
-  } catch (err) {
-    console.log(err);
-  }
-});
+app.post("/api/login", usersController.login);
 
 app.post("/api/welcome", auth, (req, res) => {
   res.status(200).send(`Welcome! ${req.user}`);
