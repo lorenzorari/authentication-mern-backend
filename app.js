c2d2ca0f1f9a2e4253473b17c4a4a85fa7bce207
Lorenzo <lorenzofg24@gmail.com>
2021-10-23 01:12:23 +0200

    Implementing getMe()

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 require("dotenv").config();
 require("./config/database").connect();
-const express = require("express");
 
+const express = require("express");
 const app = express();
+const withAuth = require("./middleware/auth");
+const usersController = require("./controllers/users");
 
 app.use(express.json());
 
-const auth = require("./middleware/auth");
-const usersController = require("./controllers/users");
+app.post("/api/users/register", usersController.register);
 
-app.post("/api/register", usersController.register);
+app.post("/api/users/login", usersController.login);
 
-app.post("/api/login", usersController.login);
-
-app.post("/api/welcome", auth, (req, res) => {
-  res.status(200).send(`Welcome! ${req.user}`);
-});
+app.post("/api/users/me", withAuth, usersController.getMe);
 
 module.exports = app;
